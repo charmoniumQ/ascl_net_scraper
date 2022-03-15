1104dde2d7d7b189b1f7810e03278a82ebc92bde
Samuel Grayson <sam@samgrayson.me>
2022-03-15 15:29:23 -0500

    Fix __main__ for pytest

--- ascl_net_scraper/__main__.py
+++ ascl_net_scraper/__main__.py
@@ -1,24 +1,28 @@
 import logging
+import os
 
 from tqdm import tqdm
 
 from .main import scrape_index_list, scrape_details
 
-logger = logging.getLogger("charmonium.freeze")
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler("freeze.log")
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(logging.Formatter("%(asctime)s: %(message)s"))
-logger.addHandler(fh)
+# __name__ == "__main__" is needed so pytest ignores this.
+if __name__ == "__main__":
+    logger = logging.getLogger("charmonium.freeze")
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler("freeze.log")
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(fh)
+    logger.debug("Program {}", os.getpid())
 
-logger = logging.getLogger("charmonium.cache.ops")
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler("cache.log")
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(logging.Formatter("%(asctime)s: %(message)s"))
-logger.addHandler(fh)
+    logger = logging.getLogger("charmonium.cache.ops")
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler("cache.log")
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(fh)
+    logger.debug("Program {}", os.getpid())
 
-
-records = scrape_index_list(10)
-for record in tqdm(records, total=len(records)):
-    detailed_record = scrape_details(record.details_url)
+    records = scrape_index_list(10)
+    for record in tqdm(records, total=len(records)):
+        detailed_record = scrape_details(record.details_url)
