3cfebf87976b4394d81aae15d69552cfed58ea3e
Samuel Grayson <sam@samgrayson.me>
2022-03-17 15:21:57 -0500

    Bump version: 0.3.0 → 0.3.1

--- ascl_net_scraper/__init__.py
+++ ascl_net_scraper/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 from .lib import CodeRecord as CodeRecord
 from .lib import DetailedCodeRecord as DetailedCodeRecord
 from .lib import scrape_details as scrape_details
